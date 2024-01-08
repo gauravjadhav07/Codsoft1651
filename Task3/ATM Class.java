66ec1c01f4f7067a78b894f605a5764e4b76b7f2
gauravjadhav07 <155989633+gauravjadhav07@users.noreply.github.com>
2024-01-08 21:03:31 +0530

    Create ATM Class.java

---
+++ Task3/ATM Class.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ATM {
+    private BankAccount account;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            displayMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    withdraw(scanner);
+                    break;
+                case 2:
+                    deposit(scanner);
+                    break;
+                case 3:
+                    checkBalance();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM.");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+}
